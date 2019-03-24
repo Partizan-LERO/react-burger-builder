3fe14564fd9ffac63bf464dd0813494b6accc973
Sergey Samoylenko <Partizan-LERO@yandex.ru>
2019-03-24 19:03:41 +0200

    added sidedrawer

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad'},
+  { label: 'Meat', type: 'meat'},
+  { label: 'Bacon', type: 'bacon'},
+  { label: 'Cheese', type: 'cheese'}
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+    {controls.map(ctrl => (
+      <BuildControl
+        disabled={props.disabled[ctrl.type]}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        type={ctrl.type}
+        key={ctrl.label}
+        label={ctrl.label}/>
+    ))}
+    <button disabled={!props.purchasable}
+            onClick={props.ordered}
+            className={classes.OrderButton}>ORDER NOW</button>
+  </div>
+);
+
+
+export default buildControls;
